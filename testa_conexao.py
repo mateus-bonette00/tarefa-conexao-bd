d3a189d14683b45e710ff13cd352caa2d5d7c6b2
mateus.bonette00 <mateus.bonette00@gmail.com>
2025-04-15 10:48:35 -0300

    [DELETE] Arquivo de teste de conexão deletado

--- testa_conexao.py
+++
@@ -1,9 +0,0 @@
-from database.conexao import conectar
-
-try:
-    conn = conectar()
-    print("Conexão realizada com sucesso!")
-    conn.close()
-except Exception as e:
-    print("Erro na conexão com o banco de dados:")
-    print(e)
